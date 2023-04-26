b166242fa9eae3b66cb3a01132d03130797be036
david papin <david.papin@imerir.com>
2023-04-26 08:09:12 +0200

    Update Carte.kt

--- app/src/main/java/com/example/monstrehunter/Carte.kt
+++ app/src/main/java/com/example/monstrehunter/Carte.kt
@@ -3,6 +3,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Bundle
 import android.view.View
 import java.util.*
 
@@ -11,24 +12,35 @@ class Carte(context: Context) : View(context) {
     private val paint = Paint()
     private val obstacles = ArrayList<Obstacle>()
 
-    class Obstacle (
+    class Obstacle(
         val x: Int,
         val y: Int,
         val longeur: Int,
         val largeur: Int,
         val color: Int
-            ) {
+    ) {
         fun draw(canvas: Canvas) {
-            canvas.drawRect(x.toFloat(), y.toFloat(), (x + longeur).toFloat(),
-                (y + largeur).toFloat(), color)
+            canvas.drawRect(
+                x.toFloat(), y.toFloat(), (x + longeur).toFloat(),
+                (y + largeur).toFloat(), color
+            )
         }
 
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carte)
 
+    }
 
-}
+    private fun Canvas.drawRect(
+        toFloat: Float,
+        toFloat1: Float,
+        toFloat2: Float,
+        toFloat3: Float,
+        color: Int
+    ) {
 
-private fun Canvas.drawRect(toFloat: Float, toFloat1: Float, toFloat2: Float, toFloat3: Float, color: Int) {
-
+    }
 }
